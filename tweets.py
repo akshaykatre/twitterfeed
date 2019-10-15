91ad9701c28988a7a58cd91f02c6d0ce8c796315
Katre <KatreAR@rabobank.corp>
2019-10-15 18:13:47 +0200

    using streaming service from tweepy

--- tweets.py
+++ tweets.py
@@ -2,46 +2,61 @@ from keys import auth
 from tweepy import Stream, StreamListener
 import json 
 import pdb
+import tweepy
 
-file = open("Output.csv", "w")
-file.write("X,Y\n")
+class StreamListener(tweepy.StreamListener):
+    """tweepy.StreamListener is a class provided by tweepy used to access
+    the Twitter Streaming API to collect tweets in real-time.
+    """
 
+    def on_connect(self):
+        """Called when the connection is made"""
+        
+        print("You're connected to the streaming server.")
 
-data_list = []
-count = 0
-
-class listener(StreamListener):
+    def on_error(self, status_code):
+        """This is called when an error occurs"""
+        
+        print('Error: ' + repr(status_code))
+        return False
 
     def on_data(self, data):
-        global count
+        """This will be called each time we receive stream data"""
+        
+       # client = MongoClient()
 
-        #How many tweets you want to find, could change to time based
-        if count <= 10:
-            json_data = json.loads(data)
-           # pdb.set_trace()
-            try:
-                coords = json_data["coordinates"]
-            except KeyError:
-                coords = None
-            if coords is not None:
-               print(coords["coordinates"])
-               lon = coords["coordinates"][0]
-               lat = coords["coordinates"][1]
+        # I stored the tweet data in a database called 'training_tweets' in MongoDB, if 
+        # 'training_tweets' does not exist it will be created for you.
+        #db = client.training_tweets
 
-               data_list.append(json_data)
+        # Decode JSON
+        datajson = json.loads(data)
+       # pdb.set_trace()
+        # I'm only storing tweets in English. I stored the data for these tweets in a collection 
+        # called 'training_tweets_collection' of the 'training_tweets' database. If 
+        # 'training_tweets_collection' does not exist it will be created for you. 
+        if "lang" in datajson:
+            #db.training_tweets_collection.insert_one(datajson)
+            print(datajson)
 
-               file.write(str(lon) + ",")
-               file.write(str(lat) + "\n")
+if __name__ == "__main__":
 
-               count += 1
-            return True
-        else:
-            file.close()
-            return False
+    # These are provided to you through the Twitter API after you create a account
+    # consumer_key = keys.consumer_key
+    # consumer_secret = keys.consumer_secret
+    # access_token = keys.access_token
+    # access_token_secret = keys.access_token_secret
 
-    def on_error(self, status):
-        print(status)
+    # auth1 = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    # auth1.set_access_token(access_token, access_token_secret)
+    #auth
+    # LOCATIONS are the longitude, latitude coordinate corners for a box that restricts the 
+    # geographic area from which you will stream tweets. The first two define the southwest
+    # corner of the box and the second two define the northeast corner of the box. 
+    LOCATIONS = [-124.7771694, 24.520833, -66.947028, 49.384472,        # Contiguous US
+                 -164.639405, 58.806859, -144.152365, 71.76871,         # Alaska
+                 -160.161542, 18.776344, -154.641396, 22.878623]        # Hawaii
 
-twitterStream = Stream(auth, listener())
-#What you want to search for here
-twitterStream.filter(track=["#BoyWithLuv"])
+    stream_listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
+    stream = tweepy.Stream(auth=auth, listener=stream_listener, tweet_mode='extended')
+    stream.filter(track=['#Rabobank', 'negative interest', '#interestrate'] )
