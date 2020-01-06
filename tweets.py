a1ebb0140f7a441c98d755d34e6e97393ee6759d
akshaykatre@gmail.com <akshaykatre>
2020-01-06 21:37:46 +0100

    Forgot to commit the tweets file

--- tweets.py
+++ tweets.py
@@ -3,6 +3,7 @@ from tweepy import Stream, StreamListener
 import json 
 import pdb
 import tweepy
+from pymongo import MongoClient
 
 class StreamListener(tweepy.StreamListener):
     """tweepy.StreamListener is a class provided by tweepy used to access
@@ -23,20 +24,25 @@ class StreamListener(tweepy.StreamListener):
     def on_data(self, data):
         """This will be called each time we receive stream data"""
         
-       # client = MongoClient()
-
+        client = MongoClient()
+        #print(type(data))
+        #print(data)
+        #print(data['text'])
         # I stored the tweet data in a database called 'training_tweets' in MongoDB, if 
         # 'training_tweets' does not exist it will be created for you.
         #db = client.training_tweets
-
+        db = client.tweetsdata
         # Decode JSON
         datajson = json.loads(data)
+        print(type(datajson))
+        #print(datajson)
        # pdb.set_trace()
         # I'm only storing tweets in English. I stored the data for these tweets in a collection 
         # called 'training_tweets_collection' of the 'training_tweets' database. If 
         # 'training_tweets_collection' does not exist it will be created for you. 
         if "lang" in datajson:
-            #db.training_tweets_collection.insert_one(datajson)
+            db.training_tweets_collection.insert_one(datajson)
+            db.tweets.insert_one(datajson)
             print(datajson)
 
 if __name__ == "__main__":
@@ -59,4 +65,4 @@ if __name__ == "__main__":
 
     stream_listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
     stream = tweepy.Stream(auth=auth, listener=stream_listener, tweet_mode='extended')
-    stream.filter(track=['#Rabobank', 'negative interest', '#interestrate'] )
+    stream.filter(track=['Rabobank', '#Rabo', '#interestrate', 'negative interest', 'banking'])
